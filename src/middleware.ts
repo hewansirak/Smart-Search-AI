8dcee0361df425630e33488119e4edfd0d4c124c
hewansirak <hewansg@gmail.com>
2024-12-14 17:47:48 +0300

    Added Rate limiting using Redis

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,44 @@
 
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+const rateLimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(10 , "60 s"),
+  analytics: true, // Using IP or sessions
+});
 
 export async function middleware(request: NextRequest) {
   try {
+    const ip = request.headers.get("x-forwarded-for") ?? "127.0.0.1";
 
-    const response = NextResponse.next();
+    const { success, limit, reset, remaining } =
+      await rateLimit.limit(ip);
 
+    // Return response with rate limit headers      
+    const response = success
+      ? NextResponse.next()
+      : NextResponse.json(
+        {erros: 'Too Many Requests'},
+        {status: 429}
+      )
+        // Add rate limit info to response headers
+
+        response.headers.set("X-RateLimit-Limit", limit.toString());
+        response.headers.set("X-RateLimit-Remaining", remaining.toString());
+        response.headers.set("X-RateLimit-Reset", reset.toString());
+           
     return response;
-
-
-
   } catch (error) {
-
-
+    console.error("Error in rate limiting middleware:", error);
+    return NextResponse.next();
   }
 }
 
